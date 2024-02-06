9620f278b1e08b606e6adedf3c73692a826a0116
rblos <jordan@mastahype.com>
2024-02-06 15:44:40 -0800

    laptop push test

--- app/src/main/java/com/example/demo1/AboutActivity.kt
+++ app/src/main/java/com/example/demo1/AboutActivity.kt
@@ -2,10 +2,17 @@ package com.example.demo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val image : ImageView = findViewById(R.id.imageView)
+        val text : TextView = findViewById(R.id.textView2)
+        val button : Button = findViewById(R.id.button4)
     }
 }
